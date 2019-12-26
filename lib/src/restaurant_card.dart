d8165e364e922b87fac82f340d0a56f085838be6
David Iglesias Teixeira <ditman@gmail.com>
2019-12-26 14:32:39 -0800

    Restaurant card WIP

--- lib/src/restaurant_card.dart
+++ lib/src/restaurant_card.dart
@@ -1,37 +1,75 @@
 import 'package:flutter/material.dart';
 
+import './model/restaurant.dart';
+
 class RestaurantCard extends StatelessWidget {
   RestaurantCard({
-    this.id, // For the Hero image and tap link
-    this.name,
-    this.cuisine,
-    this.location,
-    this.rating,
-    this.price,
-    this.imageUrl,
+    this.restaurant,
     @required Function onRestaurantPressed,
   }) : _onPressed = onRestaurantPressed;
 
-  final String id;
-  final String name;
-  final String cuisine;
-  final String location;
-  final double rating;
-  final int price;
-  final String imageUrl;
+  final Restaurant restaurant;
 
   final Function _onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => _onPressed(name),
+    return Card(
+        child: InkWell(
+      onTap: () => _onPressed(restaurant.id),
+      splashColor: Colors.blue.withAlpha(30),
       child: Container(
-        height: 200,
-        alignment: Alignment(0, 0),
-        // color: Colors.cyan,
-        child: Text(name),
+        height: 300,
+        child: Column(
+          children: <Widget>[
+            // TODO: Make this a Hero widget so we can transition to it
+            Container(
+                height: 180,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://picsum.photos/seed/${restaurant.id}/1000'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: null),
+            Expanded(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        Expanded(
+                          child: Text(
+                            restaurant.name,
+                            style: Theme.of(context).textTheme.title,
+                          ),
+                        ),
+                        Text('\$\$\$'),
+                      ],
+                    ),
+                    Expanded(
+                        child: Container(
+                      color: Colors.blue,
+                      alignment: Alignment.centerLeft,
+                      child: Text('*****'),
+                    )),
+                    Expanded(
+                        child: Container(
+                            color: Colors.red,
+                            child: Text('Thai ● Seattle'),
+                            alignment: Alignment.bottomLeft)),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-    );
+    ));
   }
 }
